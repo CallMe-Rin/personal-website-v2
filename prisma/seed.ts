68f1babd0d5ea2ced137cd249d6d63011a6acd1d
CallMe-Rin <faturahmanrinaldo@gmail.com>
2026-04-16 01:18:36 +0700

    refactor: seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,28 +1,25 @@
 import { PrismaClient } from "@prisma/client";
-import { education, experience, project } from "../src/data/data ";
+import { education, experience, project } from "../src/data/data";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  // EDUCATION
   await prisma.education.createMany({
     data: education,
     skipDuplicates: true,
   });
 
-  // EXPERIENCE
   await prisma.experience.createMany({
     data: experience,
     skipDuplicates: true,
   });
 
-  // Project
   await prisma.project.deleteMany();
 
-  // lalu insert ulang
   await prisma.project.createMany({
     data: project,
-};
+  });
+}
 
 main()
   .then(() => console.log("✅ Data berhasil di-import"))
